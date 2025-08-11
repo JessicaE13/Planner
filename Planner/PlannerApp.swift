d758714ac669626d63773bd7ca0a0c7012c44c1f
Jessica Estes <jessica.m.estes@gmail.com>
2025-08-11 07:41:23 -0400

    Initial Commit

---
+++ Planner/PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlannerApp.swift
+//  Planner
+//
+//  Created by Jessica Estes on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
